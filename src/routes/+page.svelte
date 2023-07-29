4c7d7288512fbbd82521d4a9707630da348c100a
Cédric Sazos <cedric.sazos@tutanota.com>
2023-07-29 20:03:55 +0200

    updated ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,18 +26,27 @@
 
 </script>
 
+<div style="position: absolute; left: 10%; top: 10%"> 
 
-<h1>Welcome to Smolassurl</h1>
-<p>Visit <a href="/">this</a> to read.</p>    
+    <h1>Welcome to Smolassurl</h1>
+    <p>Visit <a href="/">this</a> to read.</p>    
 
-<form on:submit|preventDefault={onSubmit}>
-    <label> url: </label>
-    <input type="text" bind:value={url}/>
-    <button> go </button>
-</form>
+    <form on:submit|preventDefault={onSubmit}>
+        <label for="input"> url: </label>
+        <input type="text" name="input" bind:value={url}/>
+        <button> go </button>
+    </form>
 
-{#if result != "" && valid}  
-    <p> result: {result} </p>
-{:else if !valid}
-    <p> URL invalide </p>
-{/if}
+    {#if result != "" && valid}  
+        <p> result: {result} </p>
+    {:else if !valid}
+        <p> URL invalide </p>
+    {/if}
+
+</div>
+
+<footer style="bottom: 0%; position: absolute; left:0; width: 100%; margin: 0; overflow: hidden;">
+    <div style="text-align: center;">  </div>
+</footer>
+
+
