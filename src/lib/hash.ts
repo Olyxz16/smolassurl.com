7d51dfe4e1995f111d0005bf0b8b8f3bc694c32e
Cédric Sazos <cedric.sazos@tutanota.com>
2023-08-28 09:36:58 +0200

    inverted the hashing process to reduce result size

--- src/lib/hash.ts
+++ src/lib/hash.ts
@@ -7,14 +7,21 @@ export async function hash(str: string) : Promise<string> {
 
     let val = BigInt("0x"+encode(str));
     
-    let hash : bigint = 0n;
-    hash = (a*(val)+c)%m;
-    hash ^= (hash >> 1098239n);
-    hash ^=((hash%maskone) << 698879n);
-    hash ^=((hash%masktwo) << 1497599n);
-    hash ^=(hash >> 1797118n);
+    val ^=(val >> 1797118n);
+    let revp = val^((val % masktwo) << 1497599n);
+    val = val^((revp % masktwo) << 1497599n);
+    revp = val^((val % maskone) << 698879n);
+    revp = val^((revp %maskone) << 698879n);
+    revp = val^((revp %maskone) << 698879n);
+    revp = val^((revp %maskone) << 698879n);
+    val = val^((revp %maskone) << 698879n);
+    revp = val^(val >> 1098239n);
+    revp = val^(revp >> 1098239n);
+    val = val^(revp >> 1098239n);
+    val = (ainverse*(val-c))%m;
+    if (val<0) {val += m;}
 
-    return hash.toString();
+    return val.toString();
 
 }
 
@@ -25,19 +32,11 @@ export async function dehash(str : string) : Promise<string> {
 
     let restore = BigInt(str);
 
+    restore = (a*(restore)+c)%m;
+    restore ^= (restore >> 1098239n);
+    restore ^=((restore%maskone) << 698879n);
+    restore ^=((restore%masktwo) << 1497599n);
     restore ^=(restore >> 1797118n);
-    let revp = restore^((restore % masktwo) << 1497599n);
-    restore = restore^((revp % masktwo) << 1497599n);
-    revp = restore^((restore % maskone) << 698879n);
-    revp = restore^((revp %maskone) << 698879n);
-    revp = restore^((revp %maskone) << 698879n);
-    revp = restore^((revp %maskone) << 698879n);
-    restore = restore^((revp %maskone) << 698879n);
-    revp = restore^(restore >> 1098239n);
-    revp = restore^(revp >> 1098239n);
-    restore = restore^(revp >> 1098239n);
-    restore = (ainverse*(restore-c))%m;
-    if (restore<0) {restore += m;}
 
     return decode(restore.toString(16));
 
