a7892db9e43585c8c6700cde90fc4d6f07b74f8e
Cédric Sazos <cedric.sazos@tutanota.com>
2023-08-28 19:29:29 +0200

    updated footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,8 +41,8 @@
 
 </div>
 
-<footer style="bottom: 0%; position: absolute; left:0; width: 100%; margin: 0; overflow: hidden;">
-    <div style="text-align: center;">  </div>
+<footer style="bottom: 1%; position: absolute; left:0; width: 100%; margin: 0; overflow: hidden;">
+    <div style="text-align: center;"> Made with 🍑 by <a href="https://github.com/Olyxz16"> Cédric Sazos </a> </div>
 </footer>
 
 
